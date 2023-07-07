e669e2a74ab2c7b6431e083e93c41f0463947f8d
rvial <rjvial@gmail.com>
2023-07-07 12:08:36 -0400

    CM

--- src/funcionPrincipal.jl
+++ src/funcionPrincipal.jl
@@ -202,8 +202,10 @@ function funcionPrincipal(tipoOptimizacion, codigo_predial::Union{Array{Int64,1}
         plan_optimizacion = [[0, lb_bbo, ub_bbo]]
         lb_bbo, ub_bbo = generaCotas(1, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
         push!(plan_optimizacion, [1, lb_bbo, ub_bbo])
-        lb_bbo, ub_bbo = generaCotas(6, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
-        push!(plan_optimizacion, [6, lb_bbo, ub_bbo])
+        lb_bbo, ub_bbo = generaCotas(2, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
+        push!(plan_optimizacion, [2, lb_bbo, ub_bbo])
+        lb_bbo, ub_bbo = generaCotas(5, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, 6)
+        push!(plan_optimizacion, [5, lb_bbo, ub_bbo])
         lb_bbo, ub_bbo = generaCotas(7, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, 6)
         push!(plan_optimizacion, [7, lb_bbo, ub_bbo])
 
