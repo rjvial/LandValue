4eea24b9f4ab98db7e093efb09271a5e4bf429a7
rvial <rjvial@gmail.com>
2023-07-20 16:09:01 -0400

    Cambios en bid price

--- src/ejemplo_bid_prices_uniform_nomad.jl
+++ src/ejemplo_bid_prices_uniform_nomad.jl
@@ -8,56 +8,58 @@ using LandValue, NOMAD
 # plot(xx[2:end], y1 .- y0) 
 
 function prob_lote(x, valorMercado_lote, valorInmobiliario_lote, prob_compraValorMercado, prob_compraValorInmobiliario)
-    p_lb = valorMercado_lote * (1 - prob_compraValorMercado)
-    p_ub = valorInmobiliario_lote / prob_compraValorInmobiliario
+    # Entrega probabilidad de compra de un lote en función del precio ofertado, x.
+    p_lb = valorMercado_lote * (1 - prob_compraValorMercado) #precio mínimo lote = bajo este precio con certeza se rechaza compra-venta
+    p_ub = valorInmobiliario_lote / prob_compraValorInmobiliario #precio maximo lote = sobre este precio con certeza se acepta compra-venta
     return x < p_lb ? 0 : (x > p_ub ? 1 : (x - p_lb) / (p_ub - p_lb))
 end
 function prob_lotes(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
+    # Entrega la probabilidad de compra de cada uno de los lotes que participan en una matriz de combinación
+    # C = matriz combinaciones
     numLotes = size(C, 2)
     superficie_combis = [sum(C[i,:] .* superficie_lotes) for i in eachindex(valorInmobiliario_combis)]
-    valorInmobiliarioUnit_combis = valorInmobiliario_combis ./ superficie_combis
+    valorInmobiliarioUnit_combis = valorInmobiliario_combis ./ superficie_combis # valor inmobiliario por m2 para cada combinación
     valorInmobiliario_lotes = copy(valorMercado_lotes)
     for k = 1:numLotes
-        valorInmobiliarioUnitPromedio_k =  sum(valorInmobiliarioUnit_combis[C[:,k] .== 1]) / sum(C[:,k] .== 1)
+        valorInmobiliarioUnitPromedio_k =  sum(valorInmobiliarioUnit_combis[C[:,k] .== 1]) / sum(C[:,k] .== 1) # valor inmobiliario por m2
         valorInmobiliario_lotes[k] = valorInmobiliarioUnitPromedio_k * superficie_lotes[k]
     end
-
     return prob_lote.(x, valorMercado_lotes, valorInmobiliario_lotes, prob_compraValorMercado, prob_compraValorInmobiliario)[:]
 end
 
 function prob_combis(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
+    # Entrega la probabilidad de compra conjunta de los lotes que forman cada combinación de la matriz de combinaciones
     numCombis, numLotes = size(C)
-
     probLotes = prob_lotes(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
     probCombis = [prod([C[k,i] == 1 ? probLotes[i] : 1 - probLotes[i] for i = 1:numLotes]) for k = 1:numCombis]
     return probCombis
 end
 
 function prob_compra(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C) 
-
+    # Probabilidad de compra de alguna de las combinaciones
     probCombis = prob_combis(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
     probCompra = sum( probCombis )
     return probCompra
 end
 
 function util_esp(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C) 
+    # Utilidad esperada 
     numCombis = size(C, 1)
-
     probCombis = prob_combis(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
     utilEsp = sum([ (valorInmobiliario_combis[k] - sum(C[k,:] .* x)) * probCombis[k] for k = 1:numCombis ])
     return utilEsp
 end
 
 function inv_esp(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C) 
+    # Inversión esperada
     numCombis = size(C, 1)
-
     probCombis = prob_combis(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
     invEsp = sum( [sum(C[k,:] .* x) * probCombis[k] for k = 1:numCombis ])
     return invEsp
 end
 
 function f(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C, minProb) 
-    
+    # Función para ingresar a NOMAD la función objetivo y las restricciones del problema
     probCompra = prob_compra(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
     utilEsp = util_esp(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMercado, prob_compraValorInmobiliario, superficie_lotes, C)
 
@@ -73,19 +75,22 @@ function f(x, valorMercado_lotes, valorInmobiliario_combis, prob_compraValorMerc
 end
 
 
-# Mínima probabilidad de compra 
+# Mínima probabilidad de compra exigida
 minProb = 0.0
 
 # Ingresar matriz C de combinaciones
-Ad = [0 1 1 0 0 0 0;
-      1 0 1 0 0 0 0;
-      1 1 0 1 0 0 0;
-      0 0 1 0 1 1 0;
-      0 0 0 1 0 0 1;
-      0 0 0 1 0 0 1;
-      0 0 0 0 1 1 0]
-graphMod.graphPlot(Ad)
-C = graphMod.node_combis(Ad, flag_mat = true) #matriz de Combinaciones de lotes
+# Ad = [0 1 1 0 0 0 0;
+#       1 0 1 0 0 0 0;
+#       1 1 0 0 0 0 0;
+#       0 0 0 0 1 1 0;
+#       0 0 0 1 0 0 1;
+#       0 0 0 1 0 0 1;
+#       0 0 0 0 1 1 0]
+# graphMod.graphPlot(Ad)
+C = graphMod.node_combis(Ad, flag_mat = true) #Genera matriz de Combinaciones de lotes
+# C = [1 1 0 0; 0 1 1 0; 0 1 0 1; 1 1 1 0; 1 1 0 1; 0 1 1 1]
+# C = [1 1 0 0; 0 1 1 0; 1 0 0 1; 1 1 0 1; 1 1 1 0; 1 1 1 1]
+C = [1 1 1 0; 0 1 1 1; 1 1 1 1]
 
 
 vec_sg = graphMod.getDisconnectedSubgraphs_v2(C)
