3ab7f52735d62badafa8ef8c2213e9bac68981e5
rvial <rjvial@gmail.com>
2023-07-28 16:37:44 -0400

    Cambios a seleccionador lotes

--- src/ejemplo_seleccionador_lotes.jl
+++ src/ejemplo_seleccionador_lotes.jl
@@ -29,20 +29,19 @@ M = 100000
 # 0  0  1  1  0;
 # 0  0  0  1  1]
 
-C = [1 1 0 0 0;
-     0 1 1 0 0;
-     0 0 1 1 0;
-     0 0 0 1 1;
-     1 1 1 0 0;
-     0 1 1 1 0;
-     0 0 1 1 1;
-     1 1 1 1 0;
-     0 1 1 1 1;
-     1 1 1 1 1]
+# C = [1 1 0 0 0;
+#      0 1 1 0 0;
+#      0 0 1 1 0;
+#      0 0 0 1 1;
+#      1 1 1 0 0;
+#      0 1 1 1 0;
+#      0 0 1 1 1;
+#      1 1 1 1 0;
+#      0 1 1 1 1;
+#      1 1 1 1 1]
 
 # C = [1 1 0;
 #      0 1 1;
-#      1 0 1;
 #      1 1 1]
 
 # C = [1 1 0 0; 0 1 1 0; 0 1 0 1; 1 1 1 0; 1 1 0 1; 0 1 1 1] #El problema con este es que no tiene 0s en la columna 2
@@ -51,21 +50,34 @@ C = [1 1 0 0 0;
 numCombi, numLotes = size(C) 
 
 idCombi = collect(1:numCombi)
-idLotes = collect(1:numLotes)
 m = JuMP.Model(Cbc.Optimizer)
 JuMP.set_optimizer_attribute(m, "ratioGap", 0.001)
 set_optimizer_attribute(m, "logLevel", 0)
 
-@variables(m, begin
+largo_Ck = [length(idCombi[C[:,k] .== 0]) for k=1:numLotes]
+
+if minimum(largo_Ck) >= 1
+    @variables(m, begin
+            x[i = 1:numLotes], Bin
+            z[k = 1:numLotes, j in idCombi[C[:,k] .== 0]], Bin
+        end)
+
+    @constraints(m, begin
+        C * x .>= 1
+        [k = 1:numLotes, j in idCombi[C[:,k] .== 0]], sum(C[j,:] .* x) + M*(x[k] + (1 - z[k,j])) >= sum(x)
+        [k = 1:numLotes], sum(z[k, idCombi[C[:,k] .== 0]]) >= 1  
+    end)
+else
+    @variables(m, begin
         x[i = 1:numLotes], Bin
-        z[k = 1:numLotes, j in idCombi[C[:,k] .== 0]], Bin
     end)
 
-@constraints(m, begin
-    C * x .>= 1
-    [k = 1:numLotes, j in idCombi[C[:,k] .== 0]], sum(C[j,:] .* x) + M*(x[k] + (1 - z[k,j])) >= sum(x)
-    [k = 1:numLotes], sum(z[k, idCombi[C[:,k] .== 0]]) >= 1  
-end)
+    @constraints(m, begin
+        C * x .>= 1
+    end)
+
+end
+
 
 @objective(m, Min, sum(x) )
 JuMP.optimize!(m)
@@ -73,4 +85,4 @@ JuMP.optimize!(m)
 display(termination_status(m))
 
 display(JuMP.value.(x))
-display(JuMP.value.(z))
+# display(JuMP.value.(z))
