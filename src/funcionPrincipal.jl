fe54e5fb8905083acfd8b23be1dd7533769abc02
rvial <rjvial@gmail.com>
2023-08-28 12:48:15 -0400

    Agrega template Z

--- src/funcionPrincipal.jl
+++ src/funcionPrincipal.jl
@@ -205,16 +205,19 @@ function funcionPrincipal(tipoOptimizacion, codigo_predial::Union{Array{Int64,1}
         temp_opt = 0
 
         # plan_optimizacion: [template, lb_bbo, ub_bbo]
-        lb_bbo, ub_bbo = generaCotas(0, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
-        plan_optimizacion = [[0, lb_bbo, ub_bbo]]
-        lb_bbo, ub_bbo = generaCotas(1, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
-        push!(plan_optimizacion, [1, lb_bbo, ub_bbo])
-        lb_bbo, ub_bbo = generaCotas(2, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
-        push!(plan_optimizacion, [2, lb_bbo, ub_bbo])
-        lb_bbo, ub_bbo = generaCotas(3, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, 6)
-        push!(plan_optimizacion, [3, lb_bbo, ub_bbo])
-        lb_bbo, ub_bbo = generaCotas(7, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, 6)
-        push!(plan_optimizacion, [7, lb_bbo, ub_bbo])
+        lb_bbo, ub_bbo = generaCotas(10, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
+        plan_optimizacion = [[10, lb_bbo, ub_bbo]]
+
+        # lb_bbo, ub_bbo = generaCotas(0, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
+        # plan_optimizacion = [[0, lb_bbo, ub_bbo]]
+        # lb_bbo, ub_bbo = generaCotas(1, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
+        # push!(plan_optimizacion, [1, lb_bbo, ub_bbo])
+        # lb_bbo, ub_bbo = generaCotas(2, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, dca.anchoMax)
+        # push!(plan_optimizacion, [2, lb_bbo, ub_bbo])
+        # lb_bbo, ub_bbo = generaCotas(3, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, 6)
+        # push!(plan_optimizacion, [3, lb_bbo, ub_bbo])
+        # lb_bbo, ub_bbo = generaCotas(7, default_min_pisos, floor(dcn.maxPisos[1]), V_areaEdif, sepNaves, maxDiagonal, dca.anchoMin, 6)
+        # push!(plan_optimizacion, [7, lb_bbo, ub_bbo])
 
         flag_penalizacion_residual = true
         flag_penalizacion_coefOcup = true
@@ -222,8 +225,8 @@ function funcionPrincipal(tipoOptimizacion, codigo_predial::Union{Array{Int64,1}
         flag_conSombra = true
         flag_divergenciaAncho = false
 
-        #                    0    1    2    3      4    5    6         7    8              9
-        vec_template_str = ["I", "L", "C", "lll", "V", "H", "C-flex", "S", "C-superFlex", "Cuña"]
+        #                    0    1    2    3      4    5    6         7    8              9       10
+        vec_template_str = ["I", "L", "C", "lll", "V", "H", "C-flex", "S", "C-superFlex", "Cuña", "Z"]
 
         # Chequea si se encontró la solución óptima o es necesario seguir optimizando
         function chequeaSolucion(x, f, fopt, template)
