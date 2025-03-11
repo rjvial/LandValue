74b9443f84d9f7ca140b9e4d632ab3399640ba73
rjvial <rjvial@gmail.com>
2025-03-11 19:06:40 -0300

    cambio en optimal lot

--- src/optimal_lot_selection.jl
+++ src/optimal_lot_selection.jl
@@ -1,6 +1,6 @@
-function optimal_lot_selection(C)
+function optimal_lot_selection(C, df)
 
-    numCombi, numLotes = size(C)
+    numCombi, numPredios = size(C)
 
     M = 100000
 
@@ -10,41 +10,37 @@ function optimal_lot_selection(C)
     JuMP.set_optimizer_attribute(m, "logLevel", 0)
     JuMP.set_time_limit_sec(m, 1*60.0)
 
-    largo_Ck_cero = [length(idCombi[C[:, k].==0]) for k = 1:numLotes] # numero de combis donde el lote k NO participa
-    set_lotes = collect(1:numLotes)
+    largo_Ck_cero = [length(idCombi[C[:, k].==0]) for k = 1:numPredios] # Vector que indica para cada k el numero de combis donde el predio k NO participa
+    set_predios = collect(1:numPredios)
 
     @variables(m, begin
-        x[i in set_lotes], Bin # xi = 1 si se selecciona el lote i
-        z[k in set_lotes[largo_Ck_cero.>=1], j in idCombi[C[:, k].==0]], Bin # zkj = 1 si la combi j, que exluye al lote k, es seleccionado
+        x[i in set_predios], Bin # x_i = 1 si se selecciona el predio i
+        z[k in set_predios[largo_Ck_cero.>=1], j in idCombi[C[:, k].==0]], Bin # z_kj = 1 si se selecciona el combi j que exluye el predio k
     end)
 
-    # debe haber al menos un x activo en cada combi
-    for j in idCombi # Para cada combi j,
-        @constraint(m, sum(C[j, :] .* x) .>= 1) # debe haber al menos un x activo 
-    end
+    # debe haber al menos un predio x activo en cada combi
+    @constraint(m, sum(x) .>= 1) # debe haber al menos un predio x activo 
 
-    # @constraint(m, C * x .>= 1) # debe haber al menos un x activo en cada combi
-    for k in set_lotes # Para cada lote k,
-        if largo_Ck_cero[k] >= 1  # si existe una o más combi's en las que el lote k no participa, 
-            @constraint(m, sum(z[k, idCombi[C[:, k].==0]]) >= 1) # al menos una de estas combi's que excluyen al lote k debe ser seleccionada
-            for j in idCombi[C[:, k].==0] # Para cada combi j que excluye al lote k,
-                @constraint(m, sum(C[j, :] .* x) + M * (x[k] + (1 - z[k, j])) >= sum(x)) #los lotes x seleccionados deben estar presentes simultáneamente en al menos una de estas combi's 
+    for k in set_predios # Para cada predio k,
+        if largo_Ck_cero[k] >= 1  # si existe uno o más combi's en los que el predio k NO participa, 
+            @constraint(m, sum(z[k, idCombi[C[:, k].==0]]) >= 1) # al menos uno de estos combi's que excluyen al predio k debe ser seleccionado
+            for j in idCombi[C[:, k].==0] # Para cada combi j que excluye al predio k,
+                @constraint(m, sum(C[j, :] .* x) + M * (x[k] + (1 - z[k, j])) >= sum(x)) # los predios x seleccionados deben estar presentes simultáneamente en cada uno de estos combi's 
+                # Si x_k = 0 y z_kj = 1 los x's no presentes en el combi j deben tener valor igual a 0
+                # En caso contrario (i.e. x_k = 1 || x_k = 0 & z_kj = 0) esta restriccion no impone ningun límite a los x's
             end
-            # Si xk = 1, esta restriccion no impone ningún limite a los x's
-            # Si xk = 0 y zkj = 0, esta restriccion no impone ningún limite a los x's
-            # Si xk = 0 y zkj = 1 los x seleccionados deben estar en la combi j
-        else  # si en cambio, el lote k participa en todas las combi's disponibles, 
-            @constraint(m, x[k] == 1) # entonces se debe seleccionar el lote xk
+        else  # si en cambio, el predio k participa en todos los combi's disponibles, 
+            @constraint(m, x[k] == 1) # entonces se debe seleccionar el predio k, x_k = 1
         end
     end
 
-    @objective(m, Min, sum(x))
+    @objective(m, Min, sum(x) + sum(x .* df.sup_terreno) / 10^5) # Se busca minimizar el número de predios seleccionados y la suma de las superficies de los predios seleccionados  
     JuMP.optimize!(m)
 
     if termination_status(m) == MOI.OPTIMAL
         return JuMP.value.(x.data)
     else
-        return zeros(Int, numLotes)
+        return zeros(Int, numPredios)
     end
 
     
--- src/script_9a_prediosEstrategicos.jl
+++ src/script_9a_prediosEstrategicos.jl
@@ -1,4 +1,7 @@
-using LandValue, DotEnv, DataFrames
+using LandValue, DotEnv, DataFrames, CSV
+
+fileDir = "C:\\Users\\rjvia\\.Neo4jDesktop\\relate-data\\dbmss\\dbms-8d2802c9-5882-41ce-a9e4-257030275495\\import\\"
+
 
 host = "neo4j://localhost:7687"
 user = "neo4j"
@@ -14,25 +17,44 @@ order by id_combi, codigo_predial
 df_predios_combis = neo4j_julia.cypher_to_dataframe(query_predios_combis, conn)
 unique_manzanas = sort(unique(df_predios_combis.manzana_sii))
 
+query_predios = """
+match (p:Predio)-[]-(c:Combi)
+return distinct p.codigo_predial as codigo_predial, p.sup_terreno as sup_terreno 
+order by codigo_predial
+"""
+df_predios = neo4j_julia.cypher_to_dataframe(query_predios, conn)
+
 df_manzanas = DataFrame(manzana_sii = unique_manzanas)
 df_manzanas.predios_estrategicos .= ""
+df_manzanas.num_predios_estrategicos .= 0
+
+# unique_manzanas = [1510500040]
 
 for i_m in eachindex(unique_manzanas)
     df_predios_combis_m = df_predios_combis[df_predios_combis.manzana_sii .== unique_manzanas[i_m], :]
     unique_combis_m = unique(df_predios_combis_m.id_combi)
     unique_codigo_predial_m = unique(df_predios_combis_m.codigo_predial)
+    df_predios_m = filter(row -> row.codigo_predial in unique_codigo_predial_m, df_predios)
     num_combis_m = length(unique_combis_m)
     num_codigo_predial_m = length(unique_codigo_predial_m)
     display("Trabajando en Manzana $(unique_manzanas[i_m]). Num Combis: $(num_combis_m). Num Predios: $(num_codigo_predial_m)")
 
-    C_m = zeros(Int, num_combis_m, num_codigo_predial_m)
-    for i_c in 1:num_combis_m, i_p in 1:num_codigo_predial_m
-        if any(df_predios_combis_m.id_combi .== unique_combis_m[i_c] .&& 
-               df_predios_combis_m.codigo_predial .== unique_codigo_predial_m[i_p])
-            C_m[i_c, i_p] = 1
+    if num_combis_m <= 25
+        C_m = zeros(Int, num_combis_m, num_codigo_predial_m)
+        for i_c in 1:num_combis_m, i_p in 1:num_codigo_predial_m
+            if any(df_predios_combis_m.id_combi .== unique_combis_m[i_c] .&& 
+                df_predios_combis_m.codigo_predial .== unique_codigo_predial_m[i_p])
+                C_m[i_c, i_p] = 1
+            end
         end
+        x_opt = optimal_lot_selection(C_m, df_predios_m)
+        predios_opt = string(unique_codigo_predial_m[x_opt .== 1])
+        num_predios_opt = sum(x_opt)
+        df_manzanas.predios_estrategicos[df_manzanas.manzana_sii .== unique_manzanas[i_m]] .= predios_opt
+        df_manzanas.num_predios_estrategicos[df_manzanas.manzana_sii .== unique_manzanas[i_m]] .= num_predios_opt
     end
-    x_opt = optimal_lot_selection(C_m)
-    lotes_opt = string(unique_codigo_predial_m[x_opt .== 1])
-    df_manzanas.predios_estrategicos[df_manzanas.manzana_sii .== unique_manzanas[i_m]] .= lotes_opt
 end
+
+fileDir_node = fileDir * "predios_estrategicos.csv"
+
+CSV.write(fileDir_node, df_manzanas)
